617196b28d188d7b796627ecabb96a1964091630
ShunBai <shunbai@infinite.net.cn>
2018-03-19 15:57:13 +0800

    增加自定义组件测试, 最重要的是, 授权功能史诗级的无敌!!!了

--- app.js
+++ app.js
@@ -16,11 +16,8 @@ App({
     // 获取用户信息
     wx.getSetting({
       success: res => {
-        if (this.getSettingReadyCallback) {
-          this.getSettingReadyCallback(res);
-        }
-        console.log(res.authSetting['scope.userInfo']);
-        if (res.authSetting['scope.userInfo']) {
+        let hasUserInfo = res.authSetting['scope.userInfo'];
+        if (!hasUserInfo) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
@@ -44,6 +41,9 @@ App({
               }
             }
           });
+        }
+        if (this.getSettingReadyCallback) {
+          this.getSettingReadyCallback(res);
         }
       }
     });
--- pages/event/index.js
+++ pages/event/index.js
@@ -7,6 +7,7 @@ Page(
       userInfo: {},
       hasUserInfo: false,
       canIUse: wx.canIUse('button.open-type.getUserInfo'),
+      hasDialog: false,
       obj: {
         index: 1,
         msg: 'hello whidy',
@@ -14,70 +15,66 @@ Page(
       }
     },
     onLoad: function() {
-      if (app.globalData.userInfo) {
-        this.setData({
-          userInfo: app.globalData.userInfo,
-          hasUserInfo: true
-        })
-      } else if (this.data.canIUse){
-        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-        // 所以此处加入 callback 以防止这种情况
+      app.getSettingReadyCallback = res => {
+        console.log('onload ', res);
+        // 如果授权成功过, 则执行自定义函数getUserInfo将信息写入app.globalData.userInfo和当前userInfo对象
+        if (res.authSetting['scope.userInfo']) {
+          this.getUserInfo();
+        }
         app.userInfoReadyCallback = res => {
+          console.log(res);
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      } else {
-        // 在没有 open-type=getUserInfo 版本的兼容处理
-        
-      };
-      app.getSettingReadyCallback = res => {
-        console.log(res);
+            hasUserInfo: true,
+            hasDialog: false
+          });
+          // console.log(this.data.hasUserInfo);
+        };
         app.failedCallback = res => {
           console.log(res);
-          wx.getUserInfo({
-            success: res => {
-              app.globalData.userInfo = res.userInfo
-              this.setData({
-                userInfo: res.userInfo,
-                hasUserInfo: true
-              })
-            }
-          })
-          
+          this.setData({
+            hasDialog: true,
+            userInfo: {},
+            hasUserInfo: false
+          });
+          // console.log(this.data.hasUserInfo);
         };
-        // if (!res.authSetting['scope.userInfo']) {
-        //   // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-        //   wx.getUserInfo({
-        //     success: res => {
-        //       // 可以将 res 发送给后台解码出 unionId
-        //       this.globalData.userInfo = res.userInfo;
-
-        //       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-        //       // 所以此处加入 callback 以防止这种情况
-        //       if (this.userInfoReadyCallback) {
-        //         this.userInfoReadyCallback(res);
-        //       }
-        //     }
-        //   });
-        // }
       };
-      // this.showZanDialog({
-      //   content: '必须授权',
-      //   confirmText: '重新授权'
-      // });
     },
     tapName: function(event) {
       console.log(event);
     },
     retry: function(e) {
-      
-      app.globalData.userInfo = e.detail.userInfo
+      // 兼容低版本微信, 开启授权设置页面用户手动授权
+      console.log('retry');
+      wx.openSetting({
+        success: res => {
+          // 为了兼容低版本微信, 尚未测试
+          console.log(res);
+          if (res.authSetting['scope.userInfo']) {
+            this.getUserInfo();
+          }
+        }
+      });
+    },
+    getUserInfo: function() {
+      console.log('获取信息成功，并写入全局数据和当前页面');
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo;
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+            hasDialog: false
+          });
+        }
+      });
+    },
+    closeDialog: function() {
+      // wx.setStorageSync('showUserInfoDialog',false) //考虑增加数据存储过期时间来优化未授权用户每次都会弹窗的问题?
       this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
-      })
+        hasDialog: false
+      });
     }
   })
 );
